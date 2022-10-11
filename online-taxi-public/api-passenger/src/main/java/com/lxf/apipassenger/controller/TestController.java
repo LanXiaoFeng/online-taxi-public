14a9f00533922ba5af32522b713d6f8f3909b71a
“LanXiaoFeng” <632288063@qq.com.cn>
2022-10-12 01:43:20 +0800

    qq

--- online-taxi-public/api-passenger/src/main/java/com/lxf/apipassenger/controller/TestController.java
+++ online-taxi-public/api-passenger/src/main/java/com/lxf/apipassenger/controller/TestController.java
@@ -9,7 +9,7 @@ public class TestController {
 
     @GetMapping("test")
     public String test(){
-        return "test api passenger 1111";
+        return "test api passenger 111";
     }
 
     /**
